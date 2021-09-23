47b34336a41fca46f49a63721334556a5ca58843
Crowdbotics <team@crowdbotics.com>
2021-09-23 01:01:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wispy_glitter_30762Identifier",
+  url: "https://wispy_glitter_30762Identifier.botics.co",
+  api: "https://wispy_glitter_30762Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
